9a2dd9797ac54024c2e639e0a12f54b8bacf6b5f
Niklaus28 <haojielim0228@hotmail.com>
2021-09-17 21:23:38 +0800

    Update exoarm.py

--- exoarm.py
+++ exoarm.py
@@ -8,13 +8,28 @@ from math import pi
 from time import sleep, time
 import numpy as np
 from matplotlib import pyplot as plt
-from util import math_utils
+
+log_level = 20  # DEBUG[10] INFO[20] WARNING[30] ERROR[40] CRITICAL[50]
+plot = False
 
 status = state.PENDING
 interrupted = False
 go_home = False
 
+group = None
+model = None
+robot_initialized = False
+
+gain_file = "gains/exoarm_gains.xml"
+hrdf_file = "hrdf/exoarm_hrdf.hrdf"
+user_file = "config/user1.yaml"
+
+class Point3d(object):
+    def __init__( self, x, y, z):
+        self.x, self.y, self.z = x, y, z
+
 def get_group():
+
     """
     Helper function to create a group from named modules, and set specified gains on the modules in that group.
     """
@@ -25,97 +40,142 @@ def get_group():
     sleep(2.0)
     group = lookup.get_group_from_names(families, names)
     if group is None:
-        return None
+        return None, None
 
     # Set gains
     gains_command = hebi.GroupCommand(group.size)
     try:
-        gains_command.read_gains("gains/exoarm_gains.xml")
+        gains_command.read_gains(gain_file)
     except Exception as e:
         print('Failed to read gains: {0}'.format(e))
-        return None
+        return None, None
     if not group.send_command_with_acknowledgement(gains_command):
         print('Failed to receive ack from group')
-        return None
+        return None, None
 
-    model = hebi.robot_model.import_from_hrdf("hrdf/exoarm_hrdf.hrdf")
+    model = None
+    try:
+        model = hebi.robot_model.import_from_hrdf(hrdf_file)
+    except Exception as e:
+        print('Could not load HRDF: {0}'.format(e))
+        return group, None
 
-    return group,model
+    return group, model
 
-def execute_trajectory(group, model, trajectory, feedback):
-  """
-  Helper function to actually execute the trajectory on a group of modules
-  """
-  global go_home
-  num_joints = group.size
-  command = hebi.GroupCommand(num_joints)
-  duration = trajectory.duration
+def decay_function(point,decay_rate,t):
+    #STARTING POINT = 15
+    #DECAY RATE = 0.022
+    #T = 9 , T for elbow = 3
+    starting_point = 15.0
+    decay_rate = 0.05
+    
+    array_size = 100
+    factor = 100
+    decay_formula2 = []
+    for i in range(array_size):
+        decay =  starting_point*pow((1-decay_rate),t)
+        decay_formula2.append(decay)
+        starting_point = decay
 
-  start = time()
-  t = time() - start
+    for i in range (array_size):
+        decay_formula2[i]= 1.5 + decay_formula2[i]
+    
+    #plt.plot(decay_formula2[0:50])
+    
+    index = decay_formula2[int(factor*point)]
+    return index
+    
+def exponential_stiffness(joint_error):
+    joint_base = abs(joint_error[0])
+    joint_J1 = abs(joint_error[1])
+    joint_J2 = abs(joint_error[2])
+    joint_J3 = abs(joint_error[3])
+    
+    if joint_base < 0.03:
+        base_stiffness = 5.0
+    elif joint_base >=0.03 and joint_base < 0.5:
+        base_stiffness = decay_function(abs(joint_base),0.022,9)
+    else:
+        base_stiffness = 1.1
 
-  while t < duration:
-        if (interrupted) and (not go_home):
-            home_position()
+    if joint_J1 < 0.03:
+        J1_stiffness = 8.0
+    elif joint_J1 >=0.03 and joint_J1 < 0.5:
+        J1_stiffness = decay_function(abs(joint_J1),0.022,9)
+    else:
+        J1_stiffness = 1.3
+
+    if joint_J2 < 0.03:
+        J2_stiffness = 8.0
+    elif joint_J2 >=0.03 and joint_J2 < 0.5:
+        J2_stiffness = decay_function(abs(joint_J2),0.022,6)
+    else:
+        J2_stiffness = 1.8
+
+    if joint_J3 < 0.03:
+        J3_stiffness = 12.0
+    elif joint_J3 >=0.03 and joint_J3 < 0.5:
+        J3_stiffness = decay_function(abs(joint_J3),0.022,3)
+    else:
+        J3_stiffness = 2.0
+
+    return np.expand_dims(np.array([base_stiffness,J1_stiffness,J2_stiffness,J3_stiffness]),axis=-1)
+
+
+def execute_trajectory(selected_pt,joint_error):
+    """
+    Helper function to actually execute the trajectory on a group of modules
+    """
+    global go_home
+    global status
+    global interrupted
+    global group
+    global model
+
+    if not robot_initialized:
+        raise RuntimeError("[execute_trajectory] Failed: robot not initialized")
+
+    num_joints = group.size
+    command = hebi.GroupCommand(num_joints)
+    feedback = hebi.GroupFeedback(group.size)
+
+    status = state.ACTIVE
+    while (abs(joint_error[0]) >=0.03) or (abs(joint_error[1]) >=0.03) or (abs(joint_error[2]) >=0.03) or (abs(joint_error[3]) >=0.1):
+        if (interrupted):
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            print("[ExoArmExecute] Time Cancel   =", current_time)
+            status = state.PREEMPTED
+            interrupted = False
+            return
             
         # Get feedback and update the timer
         group.get_next_feedback(reuse_fbk=feedback)
-        t = time() - start
+        current_joint_error = np.expand_dims(np.array(feedback.position),axis=-1)
+        joint_error = selected_pt - current_joint_error
+        torque_command = exponential_stiffness(current_joint_error) * current_joint_error
+        for i in range(group.size):
+            if abs(torque_command[i]) > 1.5:
+                torque_command[i] = 0.5 * torque_command[i]
+         
+        command.effort = torque_command 
+        group.send_command(command)
+        print(current_joint_error)
+        sleep(0.05)
 
-        # Get new commands from the trajectory
-        pos_cmd, vel_cmd, acc_cmd = trajectory.get_state(t)
-
-        # Calculate commanded efforts to assist with tracking the trajectory.
-        # Gravity Compensation uses knowledge of the arm's kinematics and mass to
-        # compensate for the weight of the arm. Dynamic Compensation uses the
-        # kinematics and mass to compensate for the commanded accelerations of the arm.
-        eff_cmd = math_utils.get_grav_comp_efforts(model, feedback.position, [0, 0, 1])
-        # NOTE: dynamic compensation effort computation has not yet been added to the APIs
-
-        vel_check = any(vel > 1.5 for vel in np.abs(vel_cmd))
-        eff_check = any(eff > 2.0 for eff in np.abs(eff_cmd))
-
-        if vel_check == 1 or eff_check == 1 :
-            home_position()
-            go_home = True
-        else:
-            # Fill in the command and send commands to the arm
-            command.position = pos_cmd
-            command.velocity = vel_cmd
-            command.effort = eff_cmd
-            group.send_command(command)
-
-def get_grav_comp_efforts(robot_model, positions, gravityVec):
-    # Normalize gravity vector (to 1g, or 9.8 m/s^2)
-    normed_gravity = gravityVec / np.linalg.norm(gravityVec) * 9.81
-
-    jacobians = robot_model.get_jacobians('CoM', positions)
-    # Get torque for each module
-    # comp_torque = J' * wrench_vector
-    # (for each frame, sum this quantity)
-    comp_torque = np.zeros((robot_model.dof_count, 1))
-
-    # Wrench vector
-    wrench_vec = np.zeros(6)  # For a single frame; this is (Fx/y/z, tau x/y/z)
-    num_frames = robot_model.get_frame_count('CoM')
-
-    for i in range(num_frames):
-        # Add the torques for each joint to support the mass at this frame
-        wrench_vec[0:3] = normed_gravity * robot_model.masses[i]
-        comp_torque += jacobians[i].transpose() * np.reshape(wrench_vec, (6, 1))
-
-    return np.squeeze(comp_torque)
+    status = state.SUCCEEDED
 
 def setup(xyz_targets):
+    logger.debug("[setup]")
+    logger.debug("[setup] xyz_targets: %f %f %f", xyz_targets[0], xyz_targets[1], xyz_targets[2])
     
     # Go to the XYZ positions at four corners of the box, and create a rotation matrix
     # that has the end effector point straight forward.
-    home_pos = np.expand_dims(np.array([0.603551,0.025137,0.260300]),axis=-1)
-    xyz_targets = np.concatenate((home_pos,xyz),axis=1)    
     xyz_cols = xyz_targets.shape[1]
-
+    feedback = hebi.GroupFeedback(group.size)
+    group.get_next_feedback(reuse_fbk=feedback)
     # Choose an "elbow up" initial configuration for IK
-    elbow_up_angles = [-pi/6.0, pi/3.0, pi/6.0, 0.0]
+    elbow_up_angles = feedback.position
     
     joint_targets = np.empty((group.size, xyz_cols))
     for col in range(xyz_cols):
@@ -125,52 +185,43 @@ def setup(xyz_targets):
         elbow_up_angles = ik_res_angles # reset seed after each loop, define the next initial joint angle of robot
 
     # Set up feedback object, and start logging
-    feedback = hebi.GroupFeedback(group.size)
     log_directory = 'dirs'
     log_filename = 'planar_motion'
     group.start_log(log_directory,log_filename, mkdirs=True)
 
-    waypoints = np.empty((group.size, 2))
     group.get_next_feedback(reuse_fbk=feedback)
-    waypoints[:, 0] = feedback.position
-    waypoints[:, 1] = joint_targets[:, 1]
-    time_vector = [0, 5]  # Seconds for the motion - do this slowly
-    vel = np.zeros((2,group.size))
-    acc = np.zeros((2,group.size))
-    trajectory = hebi.trajectory.create_trajectory(time_vector, waypoints,np.transpose(vel),np.transpose(acc))
+    joint_error = joint_targets- np.expand_dims(np.array(feedback.position),axis=-1)
 
     # Call helper function to execute this motion on the robot
-    execute_trajectory(group, model, trajectory, feedback)
+    try:
+        execute_trajectory(joint_targets,joint_error)
+        logger.info("[setup] robot status: %s", status.name)
+        if (status == state.SUCCEEDED):
+            logger.info("[setup] Reached target")
+    except Exception as e:
+        logger.error("[setup] exception: %s", e)
 
-    ## for more point to achieve
-    if xyz_cols < 3:
-        pass
-    else:
-        for col in range(xyz_cols-2):
-            waypoints[:, 0] = feedback.position
-            waypoints[:, 1] = joint_targets[:, col+2] 
-            vel = np.zeros((group.size,2))
-            acc = np.zeros((group.size,2))
-            trajectory = hebi.trajectory.create_trajectory(time_vector, waypoints,np.transpose(vel),np.transpose(acc))
-            execute_trajectory(group, model, trajectory, feedback)
-
-    log_file = group.stop_log()
-    hebi.util.plot_logs(log_file,'position',figure_spec=101)
-    hebi.util.plot_logs(log_file,'velocity',figure_spec=102)
-    hebi.util.plot_logs(log_file,'effort',figure_spec=103)
+    if (plot):
+        log_file = group.stop_log()
+        hebi.util.plot_logs(log_file,'position',figure_spec=101)
+        hebi.util.plot_logs(log_file,'velocity',figure_spec=102)
+        hebi.util.plot_logs(log_file,'effort',figure_spec=103)
 
 def home_position():
-    
-    group,model = get_group()
-    if group is None:
-        print('Group not found! Check that the family and name of a module on the network')
-        print('matches what is given in the source file.')
-        exit(1)
+    global go_home
+    global status
+    global group
+    global model
+    global pt_home
+    go_home = True
         
-    xyz_target = np.expand_dims(np.array([0.603551,0.025137,0.260300]),axis=-1)
+    logger.info("[home_position]")
+    xyz_target = np.expand_dims(np.array([pt_home.x, pt_home.y, pt_home.z]), axis=-1)
+    logger.debug("[home_position] xyz_targets: %f %f %f", xyz_target[0], xyz_target[1], xyz_target[2])
     xyz_col = xyz_target.shape[1]
-    elbow_up_angle = [-pi/6.0, pi/3.0, pi/6.0, 0.0]
-    print('reached home position')
+    feedback = hebi.GroupFeedback(group.size)
+    group.get_next_feedback(reuse_fbk=feedback)
+    elbow_up_angle = feedback.position
 
     joint_target = np.empty((group.size, xyz_col))
     for col in range(xyz_col):
@@ -179,20 +230,74 @@ def home_position():
         joint_target[:, col] = ik_res_angles
     
     # Set up feedback object, and start logging
-    feedback = hebi.GroupFeedback(group.size)
-    
-    waypoints = np.empty((group.size, 2))
     group.get_next_feedback(reuse_fbk=feedback)
-    waypoints[:, 0] = feedback.position
-    waypoints[:, 1] = joint_target[:, 0]
-    time_vector = [0, 5]  # Seconds for the motion - do this slowly
-    vel = np.zeros((2,group.size))
-    acc = np.zeros((2,group.size))
-    trajectory = hebi.trajectory.create_trajectory(time_vector, waypoints,np.transpose(vel),np.transpose(acc))
+    current_joint_angle = np.expand_dims(np.array(feedback.position),axis=-1)
+    joint_error = joint_target- current_joint_angle
 
     # Call helper function to execute this motion on the robot
-    execute_trajectory(group, model, trajectory, feedback)
+    try:
+        execute_trajectory(joint_target,joint_error)
+        if (status == state.SUCCEEDED):
+            status = state.PENDING
+            logger.info("[home_position] Reached home")
+    except Exception as e:
+        logger.error("[home_position] exception:\n %s", e)
 
+    go_home = False
+
+def setUserFile(file):
+    global user_file
+    user_file = file
+
+def setGainFile(file):
+    global gain_file
+    gain_file = file
+
+def setHrdfFile(file):
+    global hrdf_file
+    hrdf_file = file
+
+def LoadConfig():
+    import yaml
+    global pt_home
+
+    logger.info("user_file: %s", user_file)
+    try:
+        with open(user_file) as file:
+            config = yaml.safe_load(file)
+            pt_home = Point3d(config['home']['x'], config['home']['y'], config['home']['z'])
+    except Exception as e:
+        logger.error("[LoadConfig]:\n %s", e)
+        return False
+    return True
+
+def ExoArmHome():
+    home_position()
+    print(status.name)
+    print("Ready to execute goal")
+
+def ExoArmInit():
+    global group
+    global model
+    global robot_initialized
+
+    success = True
+
+    if not LoadConfig():
+        logger.error("Config file not loaded")
+        success = False
+
+    group, model = get_group()
+    if group is None:
+        logger.error('Group not found! Check that the family and name of a module on the network matches what is given in the source file.')
+        success = False
+
+    if model is None:
+        logger.error('Model not loaded! Check hrdf file is provided correctly.')
+        success = False
+
+    robot_initialized = success
+    return success
     
 def ExoArmTerminateAction():
     global interrupted
@@ -206,29 +311,39 @@ def ExoArmExecute(x, y, z):
     global status
     global interrupted
     global go_home
+    global robot_initialized
+
+    if (not robot_initialized):
+        print("[ExoArmExecute] Robot not initialized")
+        status = state.ABORTED
+        return status
 
     interrupted = False
-    print("\n\n")
+    print("")
     print("[ExoArmExecute] Received pt: ", x, y, z);
     status = state.RECEIVED
 
-    time.sleep(1)
+    sleep(1)
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print("[ExoArmExecute] Time Start =", current_time)
-    #status = state.ACTIVE
 
-    if (interrupted):
-        try:
-            xyz_targets = np.expand_dims(np.array([x,y,z]),axis=-1)
-            setup(xyz_targets)
+    if (int(x)==-100) and (int(y)==-100) and (int(z)==-100):
+        print("[ExoArmExecute] Go home request received")
+        home_position()
+        if (status == state.PENDING):
+            print("[ExoArmExecute] Go home request success")
             status = state.SUCCEEDED
-        except KeyboardInterrupt:
-            home_position()
-            status = state.ABORTED
-            #sys.exit()
-    else:
-        pass
+        else:
+            print("[ExoArmExecute] Go home request end in state: ", status.name)
+        return status
+
+    try:
+        xyz_targets = np.expand_dims(np.array([x,y,z]),axis=-1)
+        setup(xyz_targets)
+    except KeyboardInterrupt:
+        home_position()
+        status = state.ABORTED
 
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
@@ -236,11 +351,55 @@ def ExoArmExecute(x, y, z):
 
     return status
 
+def rosmain():
+    print("ExoArm ROS main")
+    global math_utils
+    from .util import math_utils as math_utils
+
+    global logger
+    import logging
+    formatter = logging.Formatter('%(message)s')
+    handler = logging.StreamHandler()
+    handler.setLevel(log_level)
+    handler.setFormatter(formatter)
+    logger = logging.getLogger('local_logger')
+    logger.setLevel(log_level)
+    logger.addHandler(handler)
+
 def main():
-    global status
-    home_position()
     print("ExoArm main")
-    ExoArmExecute(1.0, 1.0, 1.0)
+    global math_utils
+    from util import math_utils as math_utils
+
+    global logger
+    import logging as logger
+    logger.basicConfig(format='%(message)s', level=log_level)
+
+    global status
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gain', type=str, default=gain_file)
+    parser.add_argument('--hrdf', type=str, default=hrdf_file)
+    parser.add_argument('--user', type=str, default=user_file)
+    parser.add_argument('--plot', type=bool, default=False)
+    args = parser.parse_args()
+
+    setGainFile(args.gain)
+    setHrdfFile(args.hrdf)
+    setUserFile(args.user)
+
+    logger.info(" - gain file: %s", gain_file)
+    logger.info(" - hrdf file: %s", hrdf_file)
+    logger.info(" - user file: %s", user_file)
+
+    if not ExoArmInit():
+        logger.error("ExoArm Initialization Failed")
+        exit()
+
+    ExoArmHome()
+    input("Press Enter to continue...")
+    result = ExoArmExecute(1.0, 1.0, 1.0)
+    print("RESULT: ", result.name)
 
 if __name__ == "__main__":   
     main()
